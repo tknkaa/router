5c51cfa429c486a957b9a3da52e16bc0ec05f39f
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-12-23 16:41:56 +0900

    fix: correct record name in quiz table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -3,5 +3,5 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const quizzes = sqliteTable("quiz", {
   id: int("id").primaryKey({ autoIncrement: true }),
   tehai: text("tehai", { mode: "json" }).$type<number[]>(),
-  answers: text("tehai", { mode: "json" }).$type<number[]>(),
+  answers: text("answers", { mode: "json" }).$type<number[]>(),
 });
