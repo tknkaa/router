d54f9c0e1076ad073a45ecf640e6764bbf79b475
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-16 22:59:39 +0800

    chore: Remove SvelteKit boilerplate

--- src/lib/db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const quizzes = sqliteTable("quiz", {
-  id: int("id").primaryKey({ autoIncrement: true }),
-  tehai: text("tehai", { mode: "json" }).$type<number[]>(),
-  answers: text("answers", { mode: "json" }).$type<number[]>(),
-});
--- src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "$lib/db";
-import { quizzes } from "$lib/db/schema";
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async () => {
-  const user = await db.select().from(quizzes);
-  console.log(user);
-};
